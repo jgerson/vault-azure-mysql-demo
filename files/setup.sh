402a2b45bb59aca9411e4d299246a3464c6144cd
Jeremy Gerson <jgerson@hashicorp.com>
2018-09-25 15:05:26 -0400

    Update setup.sh

--- files/setup.sh
+++ files/setup.sh
@@ -45,16 +45,16 @@ EOF
 # Configure the Vault database secrets backend
 vault login root
 vault audit enable file file_path=/${HOME}/vault.log
-vault secrets enable database
+# vault secrets enable database
 
-vault write database/config/my-mysql-database \
-  plugin_name=mysql-database-plugin \
-  connection_url="{{username}}:{{password}}@tcp(${MYSQL_HOST}.mysql.database.azure.com:3306)/" \
-  allowed_roles="my-role" username="vaultadmin@${MYSQL_HOST}" password="vaultpw"
+# vault write database/config/my-mysql-database \
+#  plugin_name=mysql-database-plugin \
+#  connection_url="{{username}}:{{password}}@tcp(${MYSQL_HOST}.mysql.database.azure.com:3306)/" \
+#  allowed_roles="my-role" username="vaultadmin@${MYSQL_HOST}" password="vaultpw"
 
-vault write database/roles/my-role \
-  db_name=my-mysql-database \
-  creation_statements="CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT SELECT ON *.* TO '{{name}}'@'%';" default_ttl="1h" max_ttl="24h"
+# vault write database/roles/my-role \
+#  db_name=my-mysql-database \
+#  creation_statements="CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT SELECT ON *.* TO '{{name}}'@'%';" default_ttl="1h" max_ttl="24h"
 
 # Create an alias for fetching MySQL passwords, set up some env vars.
 echo "export VAULT_ADDR=http://127.0.0.1:8200" >> ${HOME}/.bashrc
