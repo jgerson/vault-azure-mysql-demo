9eb05fe53006c4c30673e5074ce2c519099282e6
Jeremy Gerson <jgerson@hashicorp.com>
2018-09-25 15:11:46 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -31,6 +31,19 @@ ssh ${var.admin_username}@${azurerm_public_ip.vault-pip.fqdn}
 # Open PuTTY and copy your hostname into the Host Name field. Use the admin
 # username and password you configured to log in.
 
+# Step 1b: Enable database role
+
+# vault secrets enable database
+
+# vault write database/config/my-mysql-database \
+#  plugin_name=mysql-database-plugin \
+#  connection_url="{{username}}:{{password}}@tcp(${MYSQL_HOST}.mysql.database.azure.com:3306)/" \
+#  allowed_roles="my-role" username="vaultadmin@${MYSQL_HOST}" password="vaultpw"
+
+# vault write database/roles/my-role \
+#  db_name=my-mysql-database \
+#  creation_statements="CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT SELECT ON *.* TO '{{name}}'@'%';" default_ttl="1h" max_ttl="24h"
+
 # Step 2: Run these commands on the remote VM:
 vault login root
 getPasswords
